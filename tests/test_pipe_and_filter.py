828fd257d6e38ec54901b5adfc70278b458ba065
Pedro Regis <pedroalencarregis@hotmail.com>
2020-04-27 11:24:10 -0300

    Refactor tests test_set_header_de_lote

--- tests/test_pipe_and_filter.py
+++ tests/test_pipe_and_filter.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 from freezegun import freeze_time
 
+# TODO: checar qual main_fields esta sendo usada aqui
 from pyCNAB240.core import Field, main_fields
 from pyCNAB240.pipe_and_filter import *
 
@@ -103,9 +104,8 @@ class CNABLinesTestCase(unittest.TestCase):
 
         self.assertEqual(expected, result)
 
-
-    def test_set_header_de_lote(self):
-        expected = '03300011E01##030#1000000140154558###################300004500000000000678############Um nome de empresa################################################################################999999992304202000000000#################################\n'
+    def test_set_header_de_lote_1(self):
+        expected = '03300011E01##030#1000000140154558###################'
 
         file_name = 'header_de_arquivo_lote_fields.csv'
         path_to_diretory = os.path.dirname(__file__)
@@ -118,9 +118,84 @@ class CNABLinesTestCase(unittest.TestCase):
 
         fields = fill_value_to_cnab(fields)
         pieces = build_pieces_of_value_to_cnab(fields)
-        result = build_cnab_lines(pieces)[0]
+        result = build_cnab_lines(pieces)[0][:52]
 
         self.assertEqual(expected, result)
+
+    def test_set_header_de_lote_2(self):
+        expected = '300004500000000000678############'
+
+        file_name = 'header_de_arquivo_lote_fields.csv'
+        path_to_diretory = os.path.dirname(__file__)
+        full_file_name = os.path.join(path_to_diretory, file_name)
+
+        fields = filter_segment(main_fields, '.1')
+
+        with freeze_time('2020-04-23'):
+            fields = set_header_de_lote(fields, full_file_name)
+
+        fields = fill_value_to_cnab(fields)
+        pieces = build_pieces_of_value_to_cnab(fields)
+        result = build_cnab_lines(pieces)[0][52:85]
+
+        self.assertEqual(expected, result)
+
+    def test_set_header_de_lote_3(self):
+        expected = 'Um nome de empresa########################################'
+
+        file_name = 'header_de_arquivo_lote_fields.csv'
+        path_to_diretory = os.path.dirname(__file__)
+        full_file_name = os.path.join(path_to_diretory, file_name)
+
+        fields = filter_segment(main_fields, '.1')
+
+        with freeze_time('2020-04-23'):
+            fields = set_header_de_lote(fields, full_file_name)
+
+        fields = fill_value_to_cnab(fields)
+        pieces = build_pieces_of_value_to_cnab(fields)
+        result = build_cnab_lines(pieces)[0][85:143]
+
+        self.assertEqual(expected, result)
+
+        '999999992304202000000000#################################\n'
+
+
+    def test_set_header_de_lote_4(self):
+        expected = '########################################'
+
+        file_name = 'header_de_arquivo_lote_fields.csv'
+        path_to_diretory = os.path.dirname(__file__)
+        full_file_name = os.path.join(path_to_diretory, file_name)
+
+        fields = filter_segment(main_fields, '.1')
+
+        with freeze_time('2020-04-23'):
+            fields = set_header_de_lote(fields, full_file_name)
+
+        fields = fill_value_to_cnab(fields)
+        pieces = build_pieces_of_value_to_cnab(fields)
+        result = build_cnab_lines(pieces)[0][143:183]
+
+        self.assertEqual(expected, result)
+
+    def test_set_header_de_lote_5(self):
+            expected = '999999992304202000000000#################################\n'
+
+            file_name = 'header_de_arquivo_lote_fields.csv'
+            path_to_diretory = os.path.dirname(__file__)
+            full_file_name = os.path.join(path_to_diretory, file_name)
+
+            fields = filter_segment(main_fields, '.1')
+
+            with freeze_time('2020-04-23'):
+                fields = set_header_de_lote(fields, full_file_name)
+
+            fields = fill_value_to_cnab(fields)
+            pieces = build_pieces_of_value_to_cnab(fields)
+            result = build_cnab_lines(pieces)[0][183:]
+
+            self.assertEqual(expected, result)
 
     def test_set_trailer_de_lote(self):
         expected = '03300015#########000008@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#####################################################################################################################\n'
