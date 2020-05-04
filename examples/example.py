2bc6b9f0809bcdd7168903fc5b8731add4e38f92
Pedro Regis <pedroalencarregis@hotmail.com>
2020-05-04 18:55:12 -0300

    All working
    Able to generate CNAB! Many refactor to be done.

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,7 @@
 import os
 
 from pyCNAB240.core import main_fields
+from pyCNAB240.csv_reader import build_dict_from_csv_P_Q_R
 from pyCNAB240.pipe_and_filter import santander
 
 
@@ -10,8 +11,14 @@ NÚMERO_LOTE_DE_SERVIÇO = 1 # G002
 path_to_diretory = os.path.dirname(__file__)
 header_de_arquivo = os.path.join(path_to_diretory, 'header_de_arquivo.csv')
 header_de_lote = os.path.join(path_to_diretory, 'header_de_lote.csv')
+csv_file_P_Q_R = os.path.join(path_to_diretory, 'data_segmentos_P_Q_R.csv')
 full_cnab_file_name = os.path.join(path_to_diretory, 'test.REM')
 
 
+# d = build_dict_from_csv_P_Q_R(csv_file_P_Q_R)
+#
+# print(d)
+
+
 santander(main_fields, BANK_NUMBER, NÚMERO_LOTE_DE_SERVIÇO,
-          header_de_arquivo, header_de_lote, full_cnab_file_name)
+          header_de_arquivo, header_de_lote, csv_file_P_Q_R, full_cnab_file_name)
