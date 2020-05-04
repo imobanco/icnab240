27ac95c2a276217bdafb1726092c06cd8393b8ff
Pedro Regis <pedroalencarregis@hotmail.com>
2020-05-04 15:08:19 -0300

    Add test to set_data_to_fields

--- tests/test_pipe_and_filter.py
+++ tests/test_pipe_and_filter.py
@@ -351,3 +351,29 @@ class CNABTestCase(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             check_given_data_identifiers(fields, patterns, data)
+
+    def test_set_data_to_fields(self):
+        fields = [Field(identifier='a'),
+                  Field(identifier='b'),
+                  Field(identifier='b'),
+                  Field(identifier='b'),
+                  Field(identifier='c'),
+                  Field(identifier='c'),
+                  Field(identifier='c'),
+                  Field(identifier='d'),
+                  ]
+        # TODO: checar porque o valor esta sendo transformado em string no processo
+        data = {'b': ['1', '2', '3'], 'c': ['X', 'Y', 'Z']}
+
+        expected = [Field(identifier='a'),
+                    Field(identifier='b', value=1),
+                    Field(identifier='b', value=2),
+                    Field(identifier='b', value=3),
+                    Field(identifier='c', value='X'),
+                    Field(identifier='c', value='Y'),
+                    Field(identifier='c', value='Z'),
+                    Field(identifier='d'),
+                    ]
+
+        result = set_data_to_fields(fields, data)
+        self.assertEqual(expected, result)
