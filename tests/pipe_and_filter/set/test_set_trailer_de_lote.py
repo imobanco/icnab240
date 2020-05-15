20f1d8f180328cf27416ff46b47ff9f717c51da1
rodrigondec <rodrigondec@gmail.com>
2020-05-15 12:32:47 -0300

    testes corrigidos e melhorados

--- tests/pipe_and_filter/set/test_set_trailer_de_lote.py
+++ tests/pipe_and_filter/set/test_set_trailer_de_lote.py
@@ -1,13 +1,17 @@
 from ...utils import CNABLinesTestCase
-from pyCNAB240.constants import MAIN_FIELDS
 from pyCNAB240.pipe_and_filter.set import set_trailer_de_lote
 
 
 class SetTrailerDeLoteTestCase(CNABLinesTestCase):
     def test_set_trailer_de_lote_1(self):
-        expected = "03300015#########000008@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+        total_lines_1_2_3_4_5 = 8
 
-        fields = set_trailer_de_lote(MAIN_FIELDS)
+        expected = "03300015#########00000" \
+                   f"{total_lines_1_2_3_4_5}" \
+                   "@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+        fields = self._main_fields
+        set_trailer_de_lote(fields)
 
         result = self.build_result(fields)[-2][:51]
 
@@ -16,7 +20,8 @@ class SetTrailerDeLoteTestCase(CNABLinesTestCase):
     def test_set_trailer_de_lote_2(self):
         expected = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
-        fields = set_trailer_de_lote(MAIN_FIELDS)
+        fields = self._main_fields
+        set_trailer_de_lote(fields)
 
         result = self.build_result(fields)[-2][51:104]
 
@@ -25,7 +30,8 @@ class SetTrailerDeLoteTestCase(CNABLinesTestCase):
     def test_set_trailer_de_lote_3(self):
         expected = "@@@@@@@@@@@@@@@@@@@######################################"
 
-        fields = set_trailer_de_lote(MAIN_FIELDS)
+        fields = self._main_fields
+        set_trailer_de_lote(fields)
 
         result = self.build_result(fields)[-2][104:161]
 
@@ -34,7 +40,8 @@ class SetTrailerDeLoteTestCase(CNABLinesTestCase):
     def test_set_trailer_de_lote_4(self):
         expected = "###############################################################################\n"
 
-        fields = set_trailer_de_lote(MAIN_FIELDS)
+        fields = self._main_fields
+        set_trailer_de_lote(fields)
 
         result = self.build_result(fields)[-2][161:]
 
