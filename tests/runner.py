758259ad61a7b011fb712439e8f14119f989208c
rodrigondec <rodrigondec@gmail.com>
2020-05-18 15:15:02 -0300

    TimedTextTestResult sendo utilizado

---
+++ tests/runner.py
@@ -0,0 +1,31 @@
+from time import time
+from unittest.runner import TextTestResult
+from unittest import TextTestRunner, TestLoader
+
+
+class TimedTextTestResult(TextTestResult):
+    def __init__(self, *args, **kwargs):
+        super(TimedTextTestResult, self).__init__(*args, **kwargs)
+        self.clocks = dict()
+        self.showAll = 1
+
+    def startTest(self, test):
+        self.clocks[test] = time()
+        super(TextTestResult, self).startTest(test)
+        if self.showAll:
+            self.stream.write(self.getDescription(test))
+            self.stream.write(" ... ")
+            self.stream.flush()
+
+    def addSuccess(self, test):
+        super(TextTestResult, self).addSuccess(test)
+        if self.showAll:
+            self.stream.writeln("time spent: %.6fs" % (time() - self.clocks[test]))
+        elif self.dots:
+            self.stream.write(".")
+            self.stream.flush()
+
+
+if __name__ == '__main__':
+    testsuite = TestLoader().discover('.')
+    TextTestRunner(resultclass=TimedTextTestResult).run(testsuite)
