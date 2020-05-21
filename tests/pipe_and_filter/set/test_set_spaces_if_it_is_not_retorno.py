2c045497c49174425578b6710679c31799f94d21
rodrigondec <rodrigondec@gmail.com>
2020-05-21 15:15:10 -0300

    adiciona testes de 'edge case' para set_spaces_if_it_is_not_retorno

--- tests/pipe_and_filter/set/test_set_spaces_if_it_is_not_retorno.py
+++ tests/pipe_and_filter/set/test_set_spaces_if_it_is_not_retorno.py
@@ -23,3 +23,43 @@ class SetSpacesIfItIsNotRetornoTestCase(MockedFillerTestCase):
 
         self.assertEqual("####", field.value_to_cnab)
         self.assertEqual("####", field.value)
+
+    def test_23(self):
+        field = Field(start=23, end=44, value=1, length=4, identifier=".5")
+
+        self.assertIsNone(field.value_to_cnab)
+
+        set_spaces_if_it_is_not_retorno([field])
+
+        self.assertIsNone(field.value_to_cnab)
+        self.assertEqual('1', field.value)
+
+    def test_24(self):
+        field = Field(start=24, end=44, value=1, length=4, identifier=".5")
+
+        self.assertIsNone(field.value_to_cnab)
+
+        set_spaces_if_it_is_not_retorno([field])
+
+        self.assertEqual("####", field.value_to_cnab)
+        self.assertEqual("####", field.value)
+
+    def test_116(self):
+        field = Field(start=116, end=44, value=1, length=4, identifier=".5")
+
+        self.assertIsNone(field.value_to_cnab)
+
+        set_spaces_if_it_is_not_retorno([field])
+
+        self.assertEqual("####", field.value_to_cnab)
+        self.assertEqual("####", field.value)
+
+    def test_117(self):
+        field = Field(start=117, end=44, value=1, length=4, identifier=".5")
+
+        self.assertIsNone(field.value_to_cnab)
+
+        set_spaces_if_it_is_not_retorno([field])
+
+        self.assertIsNone(field.value_to_cnab)
+        self.assertEqual('1', field.value)
